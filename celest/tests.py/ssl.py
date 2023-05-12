8322bccb848902bab7cc08283ccec0a54f958807
ÉoZak <100051022+EoZak@users.noreply.github.com>
2023-05-12 00:03:26 -0300

    Update ssl.py

--- celest/tests.py/ssl.py
+++ celest/tests.py/ssl.py
@@ -1,3 +1,5 @@
+from celest.utils.ssl_protection import SSL_Protection
+
 ssl_prot = SSL_Protection()
 host = 'www.google.com'
 response = ssl_prot.make_request('GET', host=host, port=443, path='/')
